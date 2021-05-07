2041ff239c5eddd97767e6d3438afce868fed182
edilenelopes <lopes_edi@yahoo.com.br>
2021-05-07 08:50:58 -0300

    modelagem api

---
+++ src/main/java/com/divaslu/ApiWishlist/ApiWishlistApplication.java
@@ -0,0 +1,13 @@
+package com.divaslu.ApiWishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWishlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWishlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divaslu/ApiWishlist/ApiWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divaslu.ApiWishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
