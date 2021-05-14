bbcfbb9f9198cc990fc7140370acb243782fcb20
DivasLu <divasslu@gmail.com>
2021-05-14 00:36:55 -0300

    Swagger produto

--- src/main/java/com/divaslu/ApiWishlist/controller/ProdutoController.java
+++ src/main/java/com/divaslu/ApiWishlist/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.divaslu.ApiWishlist.model.Produto;
 import com.divaslu.ApiWishlist.service.ProdutoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api("API REST Wishlist")//nome da api no swagger
+@CrossOrigin(origins="*" )//quem pode acessar essa documentação
 @RestController  //é quem define as rotas ou url
 @RequestMapping("/produtos")
 
@@ -27,28 +33,42 @@ public class ProdutoController {
 	
 	
 	@PostMapping
+	@ApiOperation(value = "Salva um produto")
 	public ResponseEntity<Produto> post(@RequestBody Produto produto){
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.saveProduto(produto));
 	}
 	    
 	@GetMapping
+	@ApiOperation(value = "Retorna uma lista de produtos")
 	public ResponseEntity<List<Produto>> getAll(){
 		return ResponseEntity.ok(service.getProdutos());
 		
 	}
 	
 	@GetMapping("/{id}")
+	@ApiOperation(value = "Retorna um único produto")
 	public ResponseEntity<Produto> getById(@PathVariable Long id){
-		return service.getProdutoById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+		return service.getProdutoById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 					
-	}
+}
+	
 	
 	@DeleteMapping("/{id}")
-	public void delete(@PathVariable long id) {
-		service.deleteById(id);
-	}
+	@ApiOperation(value = "Deleta um produto")
+	
+	public ResponseEntity<String> deletarProduto(@PathVariable long id){
+        try {
+        	service.deleteById(id);
+            return new ResponseEntity<>("Deletado com sucesso", HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>("Erro ao deletar produto"+ e, HttpStatus.BAD_REQUEST);
+        }
+    }
+	
+	
 	
 	@PutMapping
+	@ApiOperation(value = "Altera um produto")
 	public void put(@RequestBody Produto produto){
 	        service.updateProduto(produto);
 	}
