7a87b4d36de9147b156fb995fb3555a6af5e7035
EdileneLopes <lopes_edi@yahoo.com.br>
2021-05-18 20:25:47 -0300

    testes classe produto

--- src/test/java/com/divaslu/ApiWishlist/domain/model/DefaultValues.java
+++ src/test/java/com/divaslu/ApiWishlist/domain/model/DefaultValues.java
@@ -3,7 +3,7 @@ package com.divaslu.ApiWishlist.domain.model;
 public class DefaultValues {
 		public static final long ID = 1;
 		public static final String NOME = "Caderno";
-	    public static final String DESCRICAO = "Caderno 10 materias";
+	    public static final String DESCRICAO = "Caderno 12 materias";
 	    public static final String COR = "Branco";
 	    public static final double VALOR = 10.00;
 
